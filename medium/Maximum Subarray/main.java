a390bc0a69873f7075f51dd95a317ca8c16a6c90
traitimtrongvag <tbinh831@gmail.com>
2025-11-19 21:48:54 +0700

    update

--- medium/Maximum Subarray/main.java
+++ medium/Maximum Subarray/main.java
@@ -1,1 +1,19 @@
 // 53. Maximum Subarray
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currentSum = 0;
+        int maxSum = nums[0];
+
+        for (int x : nums) {
+            currentSum += x;
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+
+        return maxSum;
+    }
+}
