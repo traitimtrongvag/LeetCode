a522eaa674b3d8eaabae25d3b3bf780097e38a1c
traitimtrongvag <tbinh831@gmail.com>
2025-11-20 17:25:04 +0700

    update

--- easy/Plus One/main.cpp
+++ easy/Plus One/main.cpp
@@ -1,1 +1,18 @@
 // 66. Plus one
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int i = digits.size();
+        while (i > 0) {
+            i--;
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        vector<int> r = {1};
+        r.insert(r.end(), digits.begin(), digits.end());
+        return r;
+    }
+};
