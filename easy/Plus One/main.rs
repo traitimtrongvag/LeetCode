7fe2c08b95276bdc1c3ee0fda4818f9033589b80
traitimtrongvag <tbinh831@gmail.com>
2025-11-20 11:52:36 +0700

    update

--- easy/Plus One/main.rs
+++ easy/Plus One/main.rs
@@ -1,17 +1,17 @@
 // 66. Plus one
 impl Solution {
     pub fn plus_one(mut digits: Vec<i32>) -> Vec<i32> {
-    let mut i = digits.len();
-    while i > 0 {
-        i -= 1;
-        if digits[i] < 9 {
-            digits[i] += 1;
-            return digits;
-        }
-        digits[i] = 0;
-    }
-    let mut result = vec![1];
-    result.extend(digits);
-    result
-}
+	    let mut i = digits.len();
+	    while i > 0 {
+	        i -= 1;
+	        if digits[i] < 9 {
+	            digits[i] += 1;
+	            return digits;
+	        }
+	        digits[i] = 0;
+	    }
+	    let mut result = vec![1];
+	    result.extend(digits);
+	    result
+	}
 }
