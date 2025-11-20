be03e89214d526372019e6754388e0b17fd6c6e9
traitimtrongvag <tbinh831@gmail.com>
2025-11-20 17:25:57 +0700

    update

--- easy/Plus One/main.py
+++ easy/Plus One/main.py
@@ -1,1 +1,11 @@
 # 66. Plus one
+class Solution:
+    def plusOne(self, digits: list[int]) -> list[int]:
+        i = len(digits)
+        while i > 0:
+            i -= 1
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        return [1] + digits
