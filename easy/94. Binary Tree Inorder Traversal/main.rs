4b24d3090d69ed22b8662d9fc07452509c40a945
traitimtrongvag <tbinh831@gmail.com>
2025-11-21 18:38:03 +0700

    Update

---
+++ easy/94. Binary Tree Inorder Traversal/main.rs
@@ -0,0 +1,23 @@
+// 94. Binary Tree Inorder Traversal
+use std::rc::Rc;
+use std::cell::RefCell;
+
+impl Solution {
+    pub fn inorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        let mut res = vec![];
+        let mut stack = vec![];
+        let mut cur = root;
+
+        while cur.is_some() || !stack.is_empty() {
+            while let Some(node_rc) = cur {
+                stack.push(Rc::clone(&node_rc));
+                cur = node_rc.borrow().left.clone();
+            }
+            let node_rc = stack.pop().unwrap();
+            res.push(node_rc.borrow().val);
+            cur = node_rc.borrow().right.clone();
+        }
+
+        res
+    }
+}
