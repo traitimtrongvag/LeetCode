b4f7d06f68f20436d5f4ec5616357043eb7a828d
traitimtrongvag <tbinh831@gmail.com>
2025-11-21 20:18:34 +0700

    Update

--- easy/3079. Find the Sum of Encrypted Integers/main.cpp
+++ easy/3079. Find the Sum of Encrypted Integers/main.cpp
@@ -1,1 +1,42 @@
 // 3079. Find the Sum of Encrypted Integers
+class Solution {
+public:
+    int sumOfEncryptedInt(vector<int>& nums) {
+        int total = 0;
+        
+        for (int num : nums) {
+            total += encrypt(num);
+        }
+        
+        return total;
+    }
+    
+private:
+    int encrypt(int num) {
+        if (num == 0) {
+            return 0;
+        }
+        
+        int maxDigit = 0;
+        int count = 0;
+        int n = num;
+        
+        // Find the maximum digit and count digits
+        while (n > 0) {
+            int digit = n % 10;
+            if (digit > maxDigit) {
+                maxDigit = digit;
+            }
+            count++;
+            n /= 10;
+        }
+        
+        // Build the encrypted number
+        int result = 0;
+        for (int i = 0; i < count; i++) {
+            result = result * 10 + maxDigit;
+        }
+        
+        return result;
+    }
+};
