82286ab0bdf0795b89438c3b2f2d57289bd3e734
traitimtrongvag <tbinh831@gmail.com>
2025-11-22 00:38:45 +0700

    Update

--- easy/94. Binary Tree Inorder Traversal/main.java
+++ easy/94. Binary Tree Inorder Traversal/main.java
@@ -1,1 +1,18 @@
 // 94. Binary Tree Inorder Traversal
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> st = new Stack<>();
+        TreeNode cur = root;
+        while (cur != null || !st.isEmpty()) {
+            while (cur != null) {
+                st.push(cur);
+                cur = cur.left;
+            }
+            cur = st.pop();
+            res.add(cur.val);
+            cur = cur.right;
+        }
+        return res;
+    }
+}
