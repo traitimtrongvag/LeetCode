42f24e7ef8d4925dec9fe94cf717b93c9ae0e1c4
traitimtrongvag <tbinh831@gmail.com>
2025-11-22 00:39:07 +0700

    Update

--- easy/94. Binary Tree Inorder Traversal/main.cpp
+++ easy/94. Binary Tree Inorder Traversal/main.cpp
@@ -1,1 +1,19 @@
 // 94. Binary Tree Inorder Traversal
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<TreeNode*> st;
+        TreeNode* cur = root;
+        while (cur || !st.empty()) {
+            while (cur) {
+                st.push(cur);
+                cur = cur->left;
+            }
+            cur = st.top(); st.pop();
+            res.push_back(cur->val);
+            cur = cur->right;
+        }
+        return res;
+    }
+};
