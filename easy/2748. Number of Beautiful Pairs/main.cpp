8496b86d8ec1ec9080f8130ff618e25ba7563892
traitimtrongvag <tbinh831@gmail.com>
2025-11-21 20:10:56 +0700

    Update

--- easy/2748. Number of Beautiful Pairs/main.cpp
+++ easy/2748. Number of Beautiful Pairs/main.cpp
@@ -1,1 +1,34 @@
 // 2748. Number of Beautiful Pairs
+class Solution {
+public:
+    int countBeautifulPairs(vector<int>& nums) {
+        int count = 0;
+        int n = nums.size();
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int first = firstDigit(nums[i]);
+                int last = nums[j] % 10;
+                
+                if (gcd(first, last) == 1) {
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+    
+private:
+    int firstDigit(int num) {
+        while (num >= 10) {
+            num /= 10;
+        }
+        return num;
+    }
+    
+    int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+};
--- easy/2748. Number of Beautiful Pairs/main.java
+++ easy/2748. Number of Beautiful Pairs/main.java
@@ -1,1 +1,32 @@
 // 2748. Number of Beautiful Pairs
+class Solution {
+    public int countBeautifulPairs(int[] nums) {
+        int count = 0;
+        int n = nums.length;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int first = firstDigit(nums[i]);
+                int last = nums[j] % 10;
+                
+                if (gcd(first, last) == 1) {
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+    
+    private int firstDigit(int num) {
+        while (num >= 10) {
+            num /= 10;
+        }
+        return num;
+    }
+    
+    private int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+}
