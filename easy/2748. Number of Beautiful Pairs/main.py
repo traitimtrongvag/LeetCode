98ca8bebeb90887bc556342b98199c1f20405983
traitimtrongvag <tbinh831@gmail.com>
2025-11-21 20:11:08 +0700

    Update

--- easy/2748. Number of Beautiful Pairs/main.py
+++ easy/2748. Number of Beautiful Pairs/main.py
@@ -1,1 +1,25 @@
 # 2748. Number of Beautiful Pairs
+class Solution:
+    def countBeautifulPairs(self, nums: List[int]) -> int:
+        def first_digit(num):
+            while num >= 10:
+                num //= 10
+            return num
+        
+        def gcd(a, b):
+            if b == 0:
+                return a
+            return gcd(b, a % b)
+        
+        count = 0
+        n = len(nums)
+        
+        for i in range(n):
+            for j in range(i + 1, n):
+                first = first_digit(nums[i])
+                last = nums[j] % 10
+                
+                if gcd(first, last) == 1:
+                    count += 1
+        
+        return count
