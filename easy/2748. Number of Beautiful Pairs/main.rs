a53a020f6b6983908f9e46ff2cd906c5446f2cd4
traitimtrongvag <tbinh831@gmail.com>
2025-11-21 20:07:22 +0700

    Update

---
+++ easy/2748. Number of Beautiful Pairs/main.rs
@@ -0,0 +1,31 @@
+// 2748. Number of Beautiful Pairs
+impl Solution {
+    pub fn count_beautiful_pairs(nums: Vec<i32>) -> i32 {
+        fn gcd(a: i32, b: i32) -> i32 {
+            if b == 0 { a } else { gcd(b, a % b) }
+        }
+        
+        fn first_digit(mut num: i32) -> i32 {
+            while num >= 10 {
+                num /= 10;
+            }
+            num
+        }
+        
+        let mut count = 0;
+        let n = nums.len();
+        
+        for i in 0..n {
+            for j in (i + 1)..n {
+                let first = first_digit(nums[i]);
+                let last = nums[j] % 10;
+                
+                if gcd(first, last) == 1 {
+                    count += 1;
+                }
+            }
+        }
+        
+        count
+    }
+}
