e6db7d94bf78266d70daca44df88ec4e8e45d418
traitimtrongvag <tbinh831@gmail.com>
2025-11-22 17:36:14 +0700

    Update

--- easy/922. Sort Array By Parity II/main.py
+++ easy/922. Sort Array By Parity II/main.py
@@ -1,1 +1,14 @@
 # 922. Sort Array By Parity II
+class Solution:
+    def sortArrayByParityII(self, nums: List[int]) -> List[int]:
+        even = 0
+        odd = 1
+        
+        while even < len(nums) and odd < len(nums):
+            if nums[even] % 2 == 0:
+                even += 2
+            else:
+                nums[even], nums[odd] = nums[odd], nums[even]
+                odd += 2
+        
+        return nums
