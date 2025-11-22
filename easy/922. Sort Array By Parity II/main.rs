1c67835a165c48441f7d2857670eccdf8acd586d
traitimtrongvag <tbinh831@gmail.com>
2025-11-22 17:37:02 +0700

    Update

--- easy/922. Sort Array By Parity II/main.rs
+++ easy/922. Sort Array By Parity II/main.rs
@@ -1,1 +1,18 @@
 // 922. Sort Array By Parity II
+impl Solution {
+    pub fn sort_array_by_parity_ii(mut nums: Vec<i32>) -> Vec<i32> {
+        let mut even = 0;
+        let mut odd = 1;
+        
+        while even < nums.len() && odd < nums.len() {
+            if nums[even] % 2 == 0 {
+                even += 2;
+            } else {
+                nums.swap(even, odd);
+                odd += 2;
+            }
+        }
+        
+        nums
+    }
+}
