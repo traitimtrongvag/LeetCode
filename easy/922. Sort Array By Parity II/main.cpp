2688585da8fd201f615a121c757b668681af7c78
traitimtrongvag <tbinh831@gmail.com>
2025-11-22 17:35:54 +0700

    Update

--- easy/922. Sort Array By Parity II/main.cpp
+++ easy/922. Sort Array By Parity II/main.cpp
@@ -1,1 +1,19 @@
 // 922. Sort Array By Parity II
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& nums) {
+        int even = 0;
+        int odd = 1;
+        
+        while (even < nums.size() && odd < nums.size()) {
+            if (nums[even] % 2 == 0) {
+                even += 2;
+            } else {
+                swap(nums[even], nums[odd]);
+                odd += 2;
+            }
+        }
+        
+        return nums;
+    }
+};
