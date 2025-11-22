17c00293a9ae67509084296149c90a928a8884fd
traitimtrongvag <tbinh831@gmail.com>
2025-11-22 10:50:06 +0700

    Update

--- easy/3190. Find Minimum Operations to Make All Elements Divisible by Three/main.cpp
+++ easy/3190. Find Minimum Operations to Make All Elements Divisible by Three/main.cpp
@@ -1,1 +1,12 @@
 // 3190. Find Minimum Operations to Make All Elements Divisible by Three
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        int operations = 0;
+        for (int num : nums) {
+            int remainder = num % 3;
+            operations += min(remainder, 3 - remainder);
+        }
+        return operations;
+    }
+};
