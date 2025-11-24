4195204e071fb19038496860a5b78e06c651244e
tbinh831 <tbinh831@gmail.com>
2025-11-24 20:14:47 +0700

    Update

--- easy/14. Longest Common Prefix/main.c
+++ easy/14. Longest Common Prefix/main.c
@@ -1,1 +1,18 @@
 // 14. Longest Common Prefix
+char* longestCommonPrefix(char** strs, int strsSize) {
+    if (strsSize == 0) return "";
+    
+    for (int i = 0; strs[0][i]; i++) {
+        char c = strs[0][i];
+        for (int j = 1; j < strsSize; j++) {
+            if (strs[j][i] == '\0' || strs[j][i] != c) {
+                char* result = malloc(i + 1);
+                strncpy(result, strs[0], i);
+                result[i] = '\0';
+                return result;
+            }
+        }
+    }
+    
+    return strdup(strs[0]);
+}
