ac3cda2d19efe3132ab1bd536ff2870effd5c8aa
tbinh831 <tbinh831@gmail.com>
2025-11-24 23:52:19 +0700

    Update

--- easy/118. Pascal's Triangle/main.rs
+++ easy/118. Pascal's Triangle/main.rs
@@ -1,1 +1,16 @@
 // 118. Pascal's Triangle
+impl Solution {
+    pub fn generate(num_rows: i32) -> Vec<Vec<i32>> {
+        let num_rows = num_rows as usize;
+        let mut result = Vec::with_capacity(num_rows);
+        
+        for i in 0..num_rows {
+            let mut row = vec![1; i + 1];
+            for j in 1..i {
+                row[j] = result[i - 1][j - 1] + result[i - 1][j];
+            }
+            result.push(row);
+        }
+        result
+    }
+}
