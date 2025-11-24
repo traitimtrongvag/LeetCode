a75a13a70f10272194591224d7973b529a639db7
tbinh831 <tbinh831@gmail.com>
2025-11-24 21:07:55 +0700

    Update

--- easy/222. Count Complete Tree Nodes/main.cpp
+++ easy/222. Count Complete Tree Nodes/main.cpp
@@ -1,1 +1,25 @@
 // 222. Count Complete Tree Nodes
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if (!root) return 0;
+        int leftHeight = 0, rightHeight = 0;
+        TreeNode* left = root;
+        TreeNode* right = root;
+        
+        while (left) {
+            leftHeight++;
+            left = left->left;
+        }
+        while (right) {
+            rightHeight++;
+            right = right->right;
+        }
+        
+        if (leftHeight == rightHeight) {
+            return (1 << leftHeight) - 1;
+        }
+        
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+};
