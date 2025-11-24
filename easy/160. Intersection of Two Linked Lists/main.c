8b74a27757ed88fddcad7d4219bf10b20c2d5e24
tbinh831 <tbinh831@gmail.com>
2025-11-24 20:09:02 +0700

    Update

---
+++ easy/160. Intersection of Two Linked Lists/main.c
@@ -0,0 +1,12 @@
+// 160. Intersection of Two Linked Lists
+struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
+    struct ListNode *a = headA;
+    struct ListNode *b = headB;
+    
+    while (a != b) {
+        a = a ? a->next : headB;
+        b = b ? b->next : headA;
+    }
+    
+    return a;
+}
---
+++ easy/160. Intersection of Two Linked Lists/main.rb
@@ -0,0 +1,12 @@
+# 160. Intersection of Two Linked Lists
+def get_intersection_node(head_a, head_b)
+    a = head_a
+    b = head_b
+    
+    while a != b
+        a = a ? a.next : head_b
+        b = b ? b.next : head_a
+    end
+    
+    a
+end
