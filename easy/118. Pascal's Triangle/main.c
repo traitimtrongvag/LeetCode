40ea1a49074c2e98882d9b29726ad2551876106b
tbinh831 <tbinh831@gmail.com>
2025-11-24 23:52:37 +0700

    Update

--- easy/118. Pascal's Triangle/main.c
+++ easy/118. Pascal's Triangle/main.c
@@ -1,1 +1,17 @@
 // 118. Pascal's Triangle
+int** generate(int numRows, int* returnSize, int** returnColumnSizes) {
+    int** result = (int**)malloc(numRows * sizeof(int*));
+    *returnColumnSizes = (int*)malloc(numRows * sizeof(int));
+    *returnSize = numRows;
+    
+    for (int i = 0; i < numRows; i++) {
+        result[i] = (int*)malloc((i + 1) * sizeof(int));
+        (*returnColumnSizes)[i] = i + 1;
+        result[i][0] = 1;
+        result[i][i] = 1;
+        for (int j = 1; j < i; j++) {
+            result[i][j] = result[i - 1][j - 1] + result[i - 1][j];
+        }
+    }
+    return result;
+}
