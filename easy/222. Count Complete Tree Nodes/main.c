c573fdbbcf9e47396656e446251df1daa730a3e9
tbinh831 <tbinh831@gmail.com>
2025-11-24 21:08:21 +0700

    Update

--- easy/222. Count Complete Tree Nodes/main.c
+++ easy/222. Count Complete Tree Nodes/main.c
@@ -1,1 +1,22 @@
 // 222. Count Complete Tree Nodes
+int countNodes(struct TreeNode* root) {
+    if (!root) return 0;
+    int leftHeight = 0, rightHeight = 0;
+    struct TreeNode* left = root;
+    struct TreeNode* right = root;
+    
+    while (left) {
+        leftHeight++;
+        left = left->left;
+    }
+    while (right) {
+        rightHeight++;
+        right = right->right;
+    }
+    
+    if (leftHeight == rightHeight) {
+        return (1 << leftHeight) - 1;
+    }
+    
+    return 1 + countNodes(root->left) + countNodes(root->right);
+}
