26555672b39e9fb9dd47dbad1c0310938684d10a
tbinh831 <tbinh831@gmail.com>
2025-11-24 20:05:46 +0700

    Update

--- easy/160. Intersection of Two Linked Lists/main.java
+++ easy/160. Intersection of Two Linked Lists/main.java
@@ -1,1 +1,14 @@
 // 160. Intersection of Two Linked Lists
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a = headA;
+        ListNode b = headB;
+        
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+        
+        return a;
+    }
+}
