1c631702d1750a3e781fbbbe3225eec91f1a451e
tbinh831 <tbinh831@gmail.com>
2025-11-24 11:25:52 +0700

    Update

--- easy/1018. Binary Prefix Divisible By 5/man.rs
+++ easy/1018. Binary Prefix Divisible By 5/man.rs
@@ -1,1 +1,14 @@
 // 1018. Binary Prefix Divisible By 5
+impl Solution {
+    pub fn prefixes_div_by5(nums: Vec<i32>) -> Vec<bool> {
+        let mut result = Vec::with_capacity(nums.len());
+        let mut current = 0;
+        
+        for &num in &nums {
+            current = (current * 2 + num) % 5;
+            result.push(current == 0);
+        }
+        
+        result
+    }
+}
