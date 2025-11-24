7c335e12961011e053c1f1c8ea0af6dd34db5f6b
tbinh831 <tbinh831@gmail.com>
2025-11-24 20:18:29 +0700

    Update

--- medium/96. Unique Binary Search Trees/main.c
+++ medium/96. Unique Binary Search Trees/main.c
@@ -1,1 +1,14 @@
 // 96. Unique Binary Search Trees
+int numTrees(int n) {
+    int dp[n + 1];
+    dp[0] = dp[1] = 1;
+    
+    for (int i = 2; i <= n; i++) {
+        dp[i] = 0;
+        for (int j = 0; j < i; j++) {
+            dp[i] += dp[j] * dp[i - j - 1];
+        }
+    }
+    
+    return dp[n];
+}
