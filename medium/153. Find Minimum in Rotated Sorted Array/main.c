1c01d25d2caaddf74ff3db08e50ee51cfc8d2332
tbinh831 <tbinh831@gmail.com>
2025-11-24 20:38:09 +0700

    Update

--- medium/153. Find Minimum in Rotated Sorted Array/main.c
+++ medium/153. Find Minimum in Rotated Sorted Array/main.c
@@ -1,1 +1,13 @@
 // 153. Find Minimum in Rotated Sorted Array
+int findMin(int* nums, int numsSize) {
+    int left = 0, right = numsSize - 1;
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] > nums[right]) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+    return nums[left];
+}
