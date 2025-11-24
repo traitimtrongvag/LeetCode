0eabcb79b4cae99b26f1f2777b687282403f68c2
tbinh831 <tbinh831@gmail.com>
2025-11-24 20:05:02 +0700

    Update

--- easy/160. Intersection of Two Linked Lists/main.cpp
+++ easy/160. Intersection of Two Linked Lists/main.cpp
@@ -1,1 +1,15 @@
 // 160. Intersection of Two Linked Lists
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *a = headA;
+        ListNode *b = headB;
+        
+        while (a != b) {
+            a = a ? a->next : headB;
+            b = b ? b->next : headA;
+        }
+        
+        return a;
+    }
+};
