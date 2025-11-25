5112e796fc3f11e4ede23cc9baa981d759e4f32a
tbinh831 <tbinh831@gmail.com>
2025-11-25 20:01:59 +0700

    Update

--- medium/11. Container With Most Water/main.java
+++ medium/11. Container With Most Water/main.java
@@ -1,1 +1,17 @@
 // 11. Container With Most Water
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int maxArea = 0;
+        while (left < right) {
+            int area = Math.min(height[left], height[right]) * (right - left);
+            maxArea = Math.max(maxArea, area);
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+}
