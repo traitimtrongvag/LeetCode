af6fbf334c69e2f7db07c08e72476ee9a14832ba
tbinh831 <tbinh831@gmail.com>
2025-11-25 22:38:42 +0700

    Update

--- medium/1015. Smallest Integer Divisible by K/main.rs
+++ medium/1015. Smallest Integer Divisible by K/main.rs
@@ -1,1 +1,16 @@
 // 1015. Smallest Integer Divisible by K
+impl Solution {
+    pub fn smallest_repunit_div_by_k(k: i32) -> i32 {
+        if k % 2 == 0 || k % 5 == 0 {
+            return -1;
+        }
+        let mut remainder = 0;
+        for length in 1..=k {
+            remainder = (remainder * 10 + 1) % k;
+            if remainder == 0 {
+                return length;
+            }
+        }
+        -1
+    }
+}
