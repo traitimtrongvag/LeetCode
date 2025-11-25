0a05abe2351e060652143de7def7f8ccb46f960c
tbinh831 <tbinh831@gmail.com>
2025-11-25 11:09:13 +0700

    Update

--- easy/104. Maximum Depth of Binary Tree/main.rs
+++ easy/104. Maximum Depth of Binary Tree/main.rs
@@ -1,1 +1,17 @@
 // 104. Maximum Depth of Binary Tree
+use std::rc::Rc;
+use std::cell::RefCell;
+
+impl Solution {
+    pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        match root {
+            None => 0,
+            Some(node) => {
+                1 + std::cmp::max(
+                    Self::max_depth(node.borrow().left.clone()),
+                    Self::max_depth(node.borrow().right.clone())
+                )
+            }
+        }
+    }
+}
