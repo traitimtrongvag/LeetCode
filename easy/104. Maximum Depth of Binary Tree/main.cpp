29dd67b715e5cd101e72ab3684d21b8fc9c83737
tbinh831 <tbinh831@gmail.com>
2025-11-25 11:08:27 +0700

    Update

--- easy/104. Maximum Depth of Binary Tree/main.cpp
+++ easy/104. Maximum Depth of Binary Tree/main.cpp
@@ -1,1 +1,8 @@
 // 104. Maximum Depth of Binary Tree
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root) return 0;
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+    }
+};
