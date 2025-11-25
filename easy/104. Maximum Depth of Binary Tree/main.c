f905d9d118d2d8d02cba7699fe4f294345283136
tbinh831 <tbinh831@gmail.com>
2025-11-25 11:08:54 +0700

    Update

--- easy/104. Maximum Depth of Binary Tree/main.c
+++ easy/104. Maximum Depth of Binary Tree/main.c
@@ -1,1 +1,7 @@
 // 104. Maximum Depth of Binary Tree
+int maxDepth(struct TreeNode* root) {
+    if (!root) return 0;
+    int left = maxDepth(root->left);
+    int right = maxDepth(root->right);
+    return 1 + (left > right ? left : right);
+}
