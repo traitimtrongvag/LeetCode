94dbb2714eff2d7ea8c988ce7b5a7adf3a61d0b6
tbinh831 <tbinh831@gmail.com>
2025-11-25 22:49:34 +0700

    Update

--- easy/203. Remove Linked List Elements/main.rs
+++ easy/203. Remove Linked List Elements/main.rs
@@ -1,1 +1,16 @@
 // 203. Remove Linked List Elements
+impl Solution {
+    pub fn remove_elements(head: Option<Box<ListNode>>, val: i32) -> Option<Box<ListNode>> {
+        let mut dummy = Box::new(ListNode { val: 0, next: head });
+        let mut curr = &mut dummy;
+        
+        while let Some(ref mut next_node) = curr.next {
+            if next_node.val == val {
+                curr.next = next_node.next.take();
+            } else {
+                curr = curr.next.as_mut().unwrap();
+            }
+        }
+        dummy.next
+    }
+}
