9a729961a9e4e0f4c3cf8006a60b760f72df3086
tbinh831 <tbinh831@gmail.com>
2025-11-25 22:49:20 +0700

    Update

--- easy/203. Remove Linked List Elements/main.java
+++ easy/203. Remove Linked List Elements/main.java
@@ -1,1 +1,16 @@
 // 203. Remove Linked List Elements
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode curr = dummy;
+        
+        while (curr.next != null) {
+            if (curr.next.val == val) {
+                curr.next = curr.next.next;
+            } else {
+                curr = curr.next;
+            }
+        }
+        return dummy.next;
+    }
+}
