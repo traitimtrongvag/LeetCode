b640f38db016bd71773bc4c15577b96fca97157c
tbinh831 <tbinh831@gmail.com>
2025-11-25 21:20:48 +0700

    Update

--- easy/594. Longest Harmonious Subsequence/main.java
+++ easy/594. Longest Harmonious Subsequence/main.java
@@ -1,1 +1,18 @@
 // 594. Longest Harmonious Subsequence
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        for (int num : nums) {
+            freq[num]++;
+        }
+        
+        int maxLen = 0;
+        for (auto [num, count] : freq) {
+            if (freq.count(num + 1)) {
+                maxLen = max(maxLen, count + freq[num + 1]);
+            }
+        }
+        return maxLen;
+    }
+};
