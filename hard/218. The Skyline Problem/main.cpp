95161bf09438ab1e3700163418db4e411d1e8d10
tbinh831 <tbinh831@gmail.com>
2025-11-26 17:46:32 +0700

    Update

--- hard/218. The Skyline Problem/main.cpp
+++ hard/218. The Skyline Problem/main.cpp
@@ -1,1 +1,32 @@
 // 218. The Skyline Problem
+class Solution {
+public:
+    vector<vector<int>> getSkyline(vector<vector<int>>& buildings) {
+        vector<pair<int, int>> points;
+        for (auto& b : buildings) {
+            points.push_back({b[0], -b[2]});
+            points.push_back({b[1], b[2]});
+        }
+        sort(points.begin(), points.end());
+        
+        multiset<int> heights = {0};
+        vector<vector<int>> result;
+        int prev = 0;
+        
+        for (auto& p : points) {
+            int x = p.first, h = p.second;
+            if (h < 0) {
+                heights.insert(-h);
+            } else {
+                heights.erase(heights.find(h));
+            }
+            
+            int curr = *heights.rbegin();
+            if (curr != prev) {
+                result.push_back({x, curr});
+                prev = curr;
+            }
+        }
+        return result;
+    }
+};
