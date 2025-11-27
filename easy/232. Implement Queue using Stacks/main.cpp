c601bdb2b9d34ea14123d1b47bc59c8fbc6f371a
traitimtrongvag <tbinh831@gmail.com>
2025-11-27 22:47:33 +0700

    Update

--- easy/232. Implement Queue using Stacks/main.cpp
+++ easy/232. Implement Queue using Stacks/main.cpp
@@ -1,1 +1,33 @@
 // 232. Implement Queue using Stacks
+class MyQueue {
+private:
+    stack<int> input, output;
+
+public:
+    MyQueue() {}
+    
+    void push(int x) {
+        input.push(x);
+    }
+    
+    int pop() {
+        peek();
+        int val = output.top();
+        output.pop();
+        return val;
+    }
+    
+    int peek() {
+        if (output.empty()) {
+            while (!input.empty()) {
+                output.push(input.top());
+                input.pop();
+            }
+        }
+        return output.top();
+    }
+    
+    bool empty() {
+        return input.empty() && output.empty();
+    }
+};
