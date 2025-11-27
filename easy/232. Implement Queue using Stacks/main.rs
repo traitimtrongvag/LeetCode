acaf7c27854c8c3e4f7070c76f0fc3d3d787ca8e
traitimtrongvag <tbinh831@gmail.com>
2025-11-27 22:48:12 +0700

    Update

--- easy/232. Implement Queue using Stacks/main.rs
+++ easy/232. Implement Queue using Stacks/main.rs
@@ -1,1 +1,36 @@
 // 232. Implement Queue using Stacks
+struct MyQueue {
+    input: Vec<i32>,
+    output: Vec<i32>,
+}
+
+impl MyQueue {
+    fn new() -> Self {
+        MyQueue {
+            input: Vec::new(),
+            output: Vec::new(),
+        }
+    }
+    
+    fn push(&mut self, x: i32) {
+        self.input.push(x);
+    }
+    
+    fn pop(&mut self) -> i32 {
+        self.peek();
+        self.output.pop().unwrap()
+    }
+    
+    fn peek(&mut self) -> i32 {
+        if self.output.is_empty() {
+            while let Some(val) = self.input.pop() {
+                self.output.push(val);
+            }
+        }
+        *self.output.last().unwrap()
+    }
+    
+    fn empty(&self) -> bool {
+        self.input.is_empty() && self.output.is_empty()
+    }
+}
