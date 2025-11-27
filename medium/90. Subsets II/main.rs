51654cc276b695ed1cc2a99b7145f0269f1a4f2b
traitimtrongvag <tbinh831@gmail.com>
2025-11-27 22:35:57 +0700

    Update

--- medium/90. Subsets II/main.rs
+++ medium/90. Subsets II/main.rs
@@ -1,1 +1,23 @@
 // 90. Subsets II
+impl Solution {
+    pub fn subsets_with_dup(nums: Vec<i32>) -> Vec<Vec<i32>> {
+        let mut nums = nums;
+        nums.sort();
+        let mut result = Vec::new();
+        let mut current = Vec::new();
+        Self::backtrack(&nums, 0, &mut current, &mut result);
+        result
+    }
+    
+    fn backtrack(nums: &[i32], start: usize, current: &mut Vec<i32>, result: &mut Vec<Vec<i32>>) {
+        result.push(current.clone());
+        for i in start..nums.len() {
+            if i > start && nums[i] == nums[i - 1] {
+                continue;
+            }
+            current.push(nums[i]);
+            Self::backtrack(nums, i + 1, current, result);
+            current.pop();
+        }
+    }
+}
