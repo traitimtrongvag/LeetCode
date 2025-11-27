479f2ae527746c07507831b23b9291890a19a415
traitimtrongvag <tbinh831@gmail.com>
2025-11-27 15:37:27 +0700

    Update

--- medium/3381. Maximum Subarray Sum With Length Divisible by K/main.cpp
+++ medium/3381. Maximum Subarray Sum With Length Divisible by K/main.cpp
@@ -1,1 +1,25 @@
 // 3381. Maximum Subarray Sum With Length Divisible by K
+class Solution {
+public:
+    long long maxSubarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<long long> prefix(n + 1, 0);
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+        
+        vector<long long> minPrefix(k, LLONG_MAX);
+        minPrefix[0] = 0;
+        long long maxSum = LLONG_MIN;
+        
+        for (int i = 1; i <= n; i++) {
+            int rem = i % k;
+            if (minPrefix[rem] != LLONG_MAX) {
+                maxSum = max(maxSum, prefix[i] - minPrefix[rem]);
+            }
+            minPrefix[rem] = min(minPrefix[rem], prefix[i]);
+        }
+        
+        return maxSum;
+    }
+};
