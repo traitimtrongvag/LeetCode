b4378c7ce121730d99fd846e1ec6da05115f87c0
traitimtrongvag <tbinh831@gmail.com>
2025-11-28 14:34:16 +0700

    Update

--- medium/6. Zigzag Conversion/main.rs
+++ medium/6. Zigzag Conversion/main.rs
@@ -1,1 +1,27 @@
 // 6. Zigzag Conversion
+impl Solution {
+    pub fn convert(s: String, num_rows: i32) -> String {
+        if num_rows == 1 {
+            return s;
+        }
+        
+        let num_rows = num_rows as usize;
+        let mut rows: Vec<String> = vec![String::new(); num_rows.min(s.len())];
+        let mut cur_row = 0;
+        let mut going_down = false;
+        
+        for c in s.chars() {
+            rows[cur_row].push(c);
+            if cur_row == 0 || cur_row == num_rows - 1 {
+                going_down = !going_down;
+            }
+            if going_down {
+                cur_row += 1;
+            } else {
+                cur_row -= 1;
+            }
+        }
+        
+        rows.concat()
+    }
+}
