23ca40b6c950babe42b89a16b4ca870813db897b
traitimtrongvag <tbinh831@gmail.com>
2025-11-29 00:48:47 +0700

    Update

--- easy/119. Pascal's Triangle II/main.rs
+++ easy/119. Pascal's Triangle II/main.rs
@@ -1,1 +1,12 @@
 // 119. Pascal's Triangle II
+impl Solution {
+    pub fn get_row(row_index: i32) -> Vec<i32> {
+        let mut row = vec![1; (row_index + 1) as usize];
+        for i in 1..=row_index as usize {
+            for j in (1..i).rev() {
+                row[j] += row[j - 1];
+            }
+        }
+        row
+    }
+}
