4921225dff9d02089273c5a923e3524469a52dff
traitimtrongvag <tbinh831@gmail.com>
2025-11-29 00:49:21 +0700

    Update

--- easy/119. Pascal's Triangle II/main.c
+++ easy/119. Pascal's Triangle II/main.c
@@ -1,1 +1,12 @@
 // 119. Pascal's Triangle II
+int* getRow(int rowIndex, int* returnSize) {
+    *returnSize = rowIndex + 1;
+    int* row = (int*)malloc((rowIndex + 1) * sizeof(int));
+    for (int i = 0; i <= rowIndex; i++) {
+        row[i] = 1;
+        for (int j = i - 1; j > 0; j--) {
+            row[j] = row[j] + row[j - 1];
+        }
+    }
+    return row;
+}
