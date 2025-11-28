b1ed4a04216672a9004d438cc45425df67f6c2b5
traitimtrongvag <tbinh831@gmail.com>
2025-11-28 19:37:00 +0700

    Update

--- easy/168. Excel Sheet Column Title/main.c
+++ easy/168. Excel Sheet Column Title/main.c
@@ -1,1 +1,14 @@
 // 168. Excel Sheet Column Title
+char* convertToTitle(int columnNumber) {
+    char* result = (char*)malloc(8 * sizeof(char));
+    int index = 7;
+    result[index] = '\0';
+    
+    while (columnNumber > 0) {
+        columnNumber--;
+        result[--index] = 'A' + columnNumber % 26;
+        columnNumber /= 26;
+    }
+    
+    return result + index;
+}
