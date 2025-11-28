d06a1f69878edc21b287f7269a577b8ab5fcfa26
traitimtrongvag <tbinh831@gmail.com>
2025-11-28 12:52:31 +0700

    Update

--- medium/56. Merge Intervals/main.rs
+++ medium/56. Merge Intervals/main.rs
@@ -1,1 +1,25 @@
 // 56. Merge Intervals
+impl Solution {
+    pub fn merge(intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        if intervals.is_empty() {
+            return vec![];
+        }
+        
+        let mut intervals = intervals;
+        intervals.sort_by_key(|v| v[0]);
+        
+        let mut merged = Vec::new();
+        merged.push(intervals[0].clone());
+        
+        for i in 1..intervals.len() {
+            let last = merged.last_mut().unwrap();
+            if intervals[i][0] <= last[1] {
+                last[1] = last[1].max(intervals[i][1]);
+            } else {
+                merged.push(intervals[i].clone());
+            }
+        }
+        
+        merged
+    }
+}
