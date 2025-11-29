532723e94159edece3d341195fb2fa94901d36b0
traitimtrongvag <tbinh831@gmail.com>
2025-11-29 16:42:55 +0700

    Update

--- easy/136. Single Number/main.java
+++ easy/136. Single Number/main.java
@@ -1,1 +1,10 @@
 // 136. Single Number
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+}
