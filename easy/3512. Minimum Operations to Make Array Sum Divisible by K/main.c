8be4000277b4caa031dc68afe5ec6e3db784ba4a
traitimtrongvag <tbinh831@gmail.com>
2025-11-29 23:27:54 +0700

    Update

--- easy/3512. Minimum Operations to Make Array Sum Divisible by K/main.c
+++ easy/3512. Minimum Operations to Make Array Sum Divisible by K/main.c
@@ -1,1 +1,10 @@
 // 3512. Minimum Operations to Make Array Sum Divisible by K
+int minOperations(int* nums, int numsSize, int k) {
+    long long sum = 0;
+    for (int i = 0; i < numsSize; i++) {
+        sum += nums[i];
+    }
+    
+    int remainder = sum % k;
+    return remainder;
+}
