d79f12cd36a6c29a4179c5061d1efa2dd0b51496
traitimtrongvag <tbinh831@gmail.com>
2025-11-29 16:43:20 +0700

    Update

--- easy/136. Single Number/main.cpp
+++ easy/136. Single Number/main.cpp
@@ -1,1 +1,11 @@
 // 136. Single Number
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+};
