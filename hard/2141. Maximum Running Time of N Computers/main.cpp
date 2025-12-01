9e228be9c9a111359d4d7eb0172018f041841fbc
traitimtrongvag <tbinh831@gmail.com>
2025-12-01 22:12:24 +0700

    Update

--- hard/2141. Maximum Running Time of N Computers/main.cpp
+++ hard/2141. Maximum Running Time of N Computers/main.cpp
@@ -1,1 +1,25 @@
 // 2141. Maximum Running Time of N Computers
+class Solution {
+public:
+    long long maxRunTime(int n, vector<int>& batteries) {
+        long long left = 1, right = 0;
+        for (int battery : batteries) {
+            right += battery;
+        }
+        right /= n;
+        
+        while (left <= right) {
+            long long mid = left + (right - left) / 2;
+            long long total = 0;
+            for (int battery : batteries) {
+                total += min((long long)battery, mid);
+            }
+            if (total >= mid * n) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return right;
+    }
+};
