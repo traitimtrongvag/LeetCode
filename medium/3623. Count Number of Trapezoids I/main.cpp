e1da2e57912a9acff5a72e51e1925dd44bababea
traitimtrongvag <tbinh831@gmail.com>
2025-12-02 16:16:34 +0700

    Update

---
+++ medium/3623. Count Number of Trapezoids I/main.cpp
@@ -0,0 +1,24 @@
+// 3623. Count Number of Trapezoids I
+class Solution {
+public:
+    static const long long MOD = 1000000007;
+
+    int countTrapezoids(vector<vector<int>>& points) {
+        unordered_map<int,int> cntY;
+        for (auto &p : points) cntY[p[1]]++;
+
+        long long S = 0, SSq = 0;
+        for (auto &kv : cntY) {
+            long long c = kv.second;
+            if (c >= 2) {
+                long long v = c * (c - 1) / 2 % MOD;
+                S = (S + v) % MOD;
+                SSq = (SSq + v * v) % MOD;
+            }
+        }
+
+        long long res = (S * S % MOD - SSq + MOD) % MOD;
+        res = res * ((MOD + 1) / 2) % MOD; 
+        return (int)res;
+    }
+};
