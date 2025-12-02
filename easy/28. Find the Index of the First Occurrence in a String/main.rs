a52f6920673a8ce4862eac0e732782332a08ba83
traitimtrongvag <tbinh831@gmail.com>
2025-12-02 20:58:14 +0700

    Update

--- easy/28. Find the Index of the First Occurrence in a String/main.rs
+++ easy/28. Find the Index of the First Occurrence in a String/main.rs
@@ -1,1 +1,23 @@
 // 28. Find the Index of the First Occurrence in a String
+impl Solution {
+    pub fn str_str(haystack: String, needle: String) -> i32 {
+        if needle.is_empty() {
+            return 0;
+        }
+        
+        let m = haystack.len();
+        let n = needle.len();
+        
+        if m < n {
+            return -1;
+        }
+        
+        for (i, window) in haystack.as_bytes().windows(n).enumerate() {
+            if window == needle.as_bytes() {
+                return i as i32;
+            }
+        }
+        
+        -1
+    }
+}
