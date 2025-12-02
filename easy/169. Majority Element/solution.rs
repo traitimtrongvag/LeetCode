aa3c352dbb9a83598597c3d56cf9e559a50b16a4
traitimtrongvag <tbinh831@gmail.com>
2025-12-02 23:14:33 +0700

    Update

--- easy/169. Majority Element/solution.rs
+++ easy/169. Majority Element/solution.rs
@@ -1,1 +1,16 @@
 // 169. Majority Element
+impl Solution {
+    pub fn majority_element(nums: Vec<i32>) -> i32 {
+        let mut count = 0;
+        let mut candidate = 0;
+        
+        for &num in &nums {
+            if count == 0 {
+                candidate = num;
+            }
+            count += if num == candidate { 1 } else { -1 };
+        }
+        
+        candidate
+    }
+}
