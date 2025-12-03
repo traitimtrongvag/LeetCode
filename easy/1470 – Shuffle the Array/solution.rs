c69e85894ecfc27db2c493ed84803fd9f9570b08
traitimtrongvag <tbinh831@gmail.com>
2025-12-03 23:13:16 +0700

    Update

---
+++ easy/1470 – Shuffle the Array/solution.rs
@@ -0,0 +1,12 @@
+// 1470. Shuffle the Array
+impl Solution {
+	pub fn shuffle(nums: Vec<i32>, n: i32) -> Vec<i32> {
+	    let n = n as usize;
+	    let mut res = Vec::with_capacity(nums.len());
+	    for i in 0..n {
+	        res.push(nums[i]);
+	        res.push(nums[i + n]);
+	    }
+	    res
+	}
+}
