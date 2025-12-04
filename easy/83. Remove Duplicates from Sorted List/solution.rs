e15b886f14a91ad6c1fcc1993e3b181fba7b35a2
traitimtrongvag <tbinh831@gmail.com>
2025-12-04 22:06:25 +0700

    Update

--- easy/83. Remove Duplicates from Sorted List/solution.rs
+++ easy/83. Remove Duplicates from Sorted List/solution.rs
@@ -1,1 +1,24 @@
 // 83. Remove Duplicates from Sorted List
+type ListPtr = Box<ListNode>;
+type ListOpt = Option<ListPtr>;
+
+impl Solution {
+    pub fn delete_duplicates(mut head: ListOpt) -> ListOpt {
+        let mut curr_opt = head.as_mut();
+        
+        while let Some(curr) = curr_opt {
+            let mut next_opt = curr.next.take();
+            
+            while let Some(next) = next_opt.as_mut() {    
+                if next.val == curr.val { 
+                    next_opt  = next.next.take(); 
+                } else { 
+                    curr.next = next_opt;  
+                    break; 
+                }
+            }
+            curr_opt = curr.next.as_mut();
+        }
+        head
+    }
+}
