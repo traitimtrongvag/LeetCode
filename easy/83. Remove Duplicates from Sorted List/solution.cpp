d8ec9978a06bf32da736db838f5fb53c80647cd2
traitimtrongvag <tbinh831@gmail.com>
2025-12-04 22:08:29 +0700

    Update

--- easy/83. Remove Duplicates from Sorted List/solution.cpp
+++ easy/83. Remove Duplicates from Sorted List/solution.cpp
@@ -1,1 +1,17 @@
 // 83. Remove Duplicates from Sorted List
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* res = head;
+
+        while (head && head->next) {
+            if (head->val == head->next->val) {
+                head->next = head->next->next;
+            } else {
+                head = head->next;
+            }
+        }
+
+        return res;        
+    }
+};
