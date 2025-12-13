94f98b36be024c1c91e4cadfe648690e8608a901
traitimtrongvag <tbinh831@gmail.com>
2025-12-13 12:59:18 +0700

    Update

--- easy/3606. Coupon Code Validator/solution.java
+++ easy/3606. Coupon Code Validator/solution.java
@@ -1,61 +1,46 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-#include <ctype.h>
+class Solution {
+    public List<String> validateCoupons(String[] code, String[] businessLine, boolean[] isActive) {
+        List<String>res = new ArrayList();
 
-int cmp(const void* a, const void* b) {
-    return strcmp(*(char**)a, *(char**)b);
-}
+        List<String>e = new ArrayList();
+        List<String>g = new ArrayList();
+        List<String>p = new ArrayList();
+        List<String>r = new ArrayList();
+        
+        for(int i=0;i<isActive.length;i++){
+            if(isActive[i]){
+                if(businessLine[i].equals("electronics") 
+                    || businessLine[i].equals("grocery")
+                  || businessLine[i].equals("pharmacy")
+                  || businessLine[i].equals("restaurant")){
+                    if(code[i].length() == 0)continue;
+                    boolean found = true;
+                    for(int j=0;j<code[i].length();j++){
+                        char ch = code[i].charAt(j);
+                        if (!Character.isLetterOrDigit(ch) && ch != '_') {
+                            found = false;
+                            break;
+                        }
+                    }
 
-char** validateCoupons(char** code, int codeSize,
-                       char** businessLine, int businessLineSize,
-                       bool* isActive, int isActiveSize,
-                       int* returnSize) {
-
-    char** e = malloc(codeSize * sizeof(char*));
-    char** g = malloc(codeSize * sizeof(char*));
-    char** p = malloc(codeSize * sizeof(char*));
-    char** r = malloc(codeSize * sizeof(char*));
-
-    int ec=0,gc=0,pc=0,rc=0;
-
-    for (int i = 0; i < isActiveSize; i++) {
-        if (!isActive[i]) continue;
-
-        if (strcmp(businessLine[i], "electronics") &&
-            strcmp(businessLine[i], "grocery") &&
-            strcmp(businessLine[i], "pharmacy") &&
-            strcmp(businessLine[i], "restaurant"))
-            continue;
-
-        if (strlen(code[i]) == 0) continue;
-
-        bool ok = true;
-        for (int j = 0; code[i][j]; j++) {
-            if (!isalnum(code[i][j]) && code[i][j] != '_') {
-                ok = false; break;
+                    if(found){
+                        if(businessLine[i].startsWith("e"))e.add(code[i]);
+                        if(businessLine[i].startsWith("g"))g.add(code[i]);
+                        if(businessLine[i].startsWith("p"))p.add(code[i]);
+                        if(businessLine[i].startsWith("r"))r.add(code[i]);
+                    }
+                  }
             }
         }
-        if (!ok) continue;
-
-        if (businessLine[i][0] == 'e') e[ec++] = code[i];
-        if (businessLine[i][0] == 'g') g[gc++] = code[i];
-        if (businessLine[i][0] == 'p') p[pc++] = code[i];
-        if (businessLine[i][0] == 'r') r[rc++] = code[i];
+        Collections.sort(e);
+        Collections.sort(g);
+        Collections.sort(p);
+        Collections.sort(r);
+        res.addAll(e);
+        res.addAll(g);
+        res.addAll(p);
+        res.addAll(r);
+        
+        return res;
     }
-
-    qsort(e, ec, sizeof(char*), cmp);
-    qsort(g, gc, sizeof(char*), cmp);
-    qsort(p, pc, sizeof(char*), cmp);
-    qsort(r, rc, sizeof(char*), cmp);
-
-    char** res = malloc((ec+gc+pc+rc) * sizeof(char*));
-    int idx = 0;
-    for (int i=0;i<ec;i++) res[idx++] = e[i];
-    for (int i=0;i<gc;i++) res[idx++] = g[i];
-    for (int i=0;i<pc;i++) res[idx++] = p[i];
-    for (int i=0;i<rc;i++) res[idx++] = r[i];
-
-    *returnSize = idx;
-    return res;
 }
