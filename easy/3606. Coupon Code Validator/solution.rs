9562727de0f7973be7931a9e8db3762dbb9bb7ec
traitimtrongvag <tbinh831@gmail.com>
2025-12-13 13:00:47 +0700

    Update

--- easy/3606. Coupon Code Validator/solution.rs
+++ easy/3606. Coupon Code Validator/solution.rs
@@ -1,16 +1,24 @@
-let n = code.len();
-let mut xs: Vec<(String, String)> = Vec::new();
+impl Solution {
+    pub fn validate_coupons(code: Vec<String>, business_line: Vec<String>, is_active: Vec<bool>) -> Vec<String> {
+        let n = code.len();
+        let mut xs: Vec<(String, String)> = Vec::new();
 
-for i in 0..n {
-    let code = &code[i];
-    let b = &business_line[i];
-    let a = is_active[i];
+        for i in 0..n {
+            let code = &code[i];
+            let b = &business_line[i];
+            let a = is_active[i];
 
-    if a
-       && (b == "electronics" || b == "grocery" || b == "pharmacy" || b == "restaurant")
-       && !code.is_empty()
-       && code.chars().all(|c | c.is_ascii_alphanumeric() || c == '_' )
-    {
-        xs.push((b.clone(), code.clone()));
+            if a
+               && (b == "electronics" || b == "grocery" || b == "pharmacy" || b == "restaurant")
+               && !code.is_empty()
+               && code.chars().all(|c | c.is_ascii_alphanumeric() || c == '_' )
+            {
+                xs.push((b.clone(), code.clone()));
+            }
+        };
+
+        xs.sort();
+
+        xs.into_iter().map(|x| x.1).collect()
     }
-};
+}
