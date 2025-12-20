244a76bc3993aa86898c98869c7b2f957ce17abe
traitimtrongvag <tbinh831@gmail.com>
2025-12-20 20:45:35 +0700

    Update

---
+++ easy/2974. Minimum Number Game/solution.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Solution {
+    public int[] numberGame(int[] v) {
+        int n = v.length;
+        Arrays.sort(v);
+        for (int i = 0; i < n; i += 2) {
+            if (i + 1 < n) {
+                int temp = v[i];
+                v[i] = v[i + 1];
+                v[i + 1] = temp;
+            }
+        }
+        return v;
+    }
+}
